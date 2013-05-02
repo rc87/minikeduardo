327870ce685a5550f99e3848baa2b083e426e7a5
rc87 <root@eduardo-VirtualBox.(none)>
2013-05-02 12:04:13 +0200

    vamos a ver que saleee

---
+++ holamundo.java
@@ -0,0 +1,14 @@
+
+//programa hola mundo
+//autor Eduardo 
+//fecha 2/05/2013
+//Muestra el mensaje Hola mundo
+
+#include <stdio.h>
+
+
+int main(void){
+
+printf("Hola mundo remoto"\n);
+return 0;
+}
